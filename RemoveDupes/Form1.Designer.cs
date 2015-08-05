eecb4326c4adb37aa9fc2f88e7e9e26965804813
Jayson Gallardo <jayson.gallardo@amgreetings.com>
2015-08-05 09:50:45 -0500

    First version

---
+++ RemoveDupes/Form1.Designer.cs
@@ -0,0 +1,95 @@
+﻿namespace RemoveDupes
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.saveFileDialog1 = new System.Windows.Forms.SaveFileDialog();
+            this.bwProcessFiles = new System.ComponentModel.BackgroundWorker();
+            this.button1 = new System.Windows.Forms.Button();
+            this.progressBar1 = new System.Windows.Forms.ProgressBar();
+            this.label1 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "Source File";
+            // 
+            // saveFileDialog1
+            // 
+            this.saveFileDialog1.FileName = "Destination File";
+            // 
+            // bwProcessFiles
+            // 
+            this.bwProcessFiles.WorkerReportsProgress = true;
+            this.bwProcessFiles.DoWork += new System.ComponentModel.DoWorkEventHandler(this.bwProcessFiles_DoWork);
+            this.bwProcessFiles.ProgressChanged += new System.ComponentModel.ProgressChangedEventHandler(this.bwProcessFiles_ProgressChanged);
+            this.bwProcessFiles.RunWorkerCompleted += new System.ComponentModel.RunWorkerCompletedEventHandler(this.bwProcessFiles_RunWorkerCompleted);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(12, 12);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Start";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // progressBar1
+            // 
+            this.progressBar1.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Left | System.Windows.Forms.AnchorStyles.Right)));
+            this.progressBar1.Location = new System.Drawing.Point(12, 49);
+            this.progressBar1.Name = "progressBar1";
+            this.progressBar1.Size = new System.Drawing.Size(742, 23);
+            this.progressBar1.TabIndex = 2;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(385, 33);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(0, 13);
+            this.label1.TabIndex = 3;
+            this.label1.TextAlign = System.Drawing.ContentAlignment.TopCenter;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(766, 103);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.progressBar1);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Remove Duplicate Lines from File";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.SaveFileDialog saveFileDialog1;
+        private System.ComponentModel.BackgroundWorker bwProcessFiles;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.ProgressBar progressBar1;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ RemoveDupes/Form1.cs
@@ -0,0 +1,133 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace RemoveDupes
+{
+    public partial class Form1 : Form
+    {
+        string sourceFilename;
+        string destinationFilename;
+        StreamReader sourceReader;
+        StreamWriter destinationWriter;
+        int dupesCount = 0;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+        }
+
+        private void bwProcessFiles_DoWork(object sender, DoWorkEventArgs e)
+        {
+            var temp = new StreamReader(sourceFilename);
+            int numLines = 0;
+            while (!temp.EndOfStream)
+            {
+                temp.ReadLine();
+                numLines++;
+            }
+            temp.Close();
+
+            sourceReader = new StreamReader(sourceFilename);
+            destinationWriter = new StreamWriter(destinationFilename);
+
+            var linesHash = new HashSet<int>();
+            int lineNumber = 0;
+            while (!sourceReader.EndOfStream)
+            {
+                string line = sourceReader.ReadLine();
+                bwProcessFiles.ReportProgress((int)((lineNumber++ * 100) / numLines));
+                int hash = line.GetHashCode();
+                if (linesHash.Contains(hash))
+                {
+                    // if destinationWriter contains line string, continue
+                    destinationWriter.Flush();
+                    destinationWriter.Close();
+                    temp = new StreamReader(destinationFilename);
+                    bool found = false;
+                    while (!temp.EndOfStream)
+                    {
+                        if (line == temp.ReadLine())
+                        {
+                            found = true;
+                            dupesCount++;
+                            break;
+                        }
+                    }
+
+                    temp.Close();
+                    destinationWriter = new StreamWriter(destinationFilename, true);
+                    if (found)
+                    {
+                        continue;
+                    }
+                    else
+                    {
+                        destinationWriter.WriteLine(line);
+                    }
+                }
+                else
+                {
+                    linesHash.Add(hash);
+                    destinationWriter.WriteLine(line);
+                }
+            }
+        }
+
+        private void bwProcessFiles_ProgressChanged(object sender, ProgressChangedEventArgs e)
+        {
+            progressBar1.Value = e.ProgressPercentage;
+            label1.Text = e.ProgressPercentage.ToString() + "%";
+        }
+
+        private void bwProcessFiles_RunWorkerCompleted(object sender, RunWorkerCompletedEventArgs e)
+        {
+            this.Cursor = Cursors.Default;
+            sourceReader.Close();
+            destinationWriter.Flush();
+            destinationWriter.Close();
+            label1.Text = dupesCount.ToString();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (openFileDialog1.ShowDialog() == DialogResult.OK)
+            {
+                if (saveFileDialog1.ShowDialog() == DialogResult.OK)
+                {
+                    this.Cursor = Cursors.WaitCursor;
+                    sourceFilename = openFileDialog1.FileName;
+                    destinationFilename = saveFileDialog1.FileName;
+                    bwProcessFiles.RunWorkerAsync();
+                }
+            }
+        }
+
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            sourceReader.Dispose();
+            destinationWriter.Dispose();
+            base.Dispose(disposing);
+        }
+    }
+}
